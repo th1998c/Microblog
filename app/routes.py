1a50b0fd7f29540ef7db46fbe809cfca08537219
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2023-01-09 16:54:42 -0300

    criado form de login e campos validados

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template
-
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 from app import app
 
 @app.route('/')
@@ -17,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sing In', form=form)
