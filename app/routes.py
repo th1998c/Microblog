5a7320f82083af80de45a1a90ee6dbb00641ae75
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2023-01-09 14:25:15 -0300

    retornando post com for e dividindo em layout

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Thiago'}
-    return render_template('index.html', title='home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
