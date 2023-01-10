383211903b629d14831b7d33a64b3ba6e1646164
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2023-01-10 17:53:00 -0300

    hash password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 class User(db.Model):
@@ -11,6 +11,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -19,3 +24,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
