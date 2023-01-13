3c46d73dfda3492bfda38d288d11cedc43c77df6
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2023-01-13 14:44:25 -0300

    criada tela explorer retornando posts de users nao seguidos

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,7 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
     posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home Page',form=form, posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -138,3 +138,9 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
